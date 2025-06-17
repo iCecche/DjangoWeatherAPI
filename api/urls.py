da088a38cb2427c469072a9978868874cadcabfe
Alessio Ceccherini <alessioceccherini03@gmail.com>
2025-06-17 19:49:02 +0200

    Feat: add POST action to add forecast to db

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import get_forecast
+from .views import get_forecast, create_forecast
 
 urlpatterns = [
     path('get_forecast/', get_forecast, name='get_forecast'),
+    path('create_forecast/', create_forecast, name='create_forecast'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,3 @@
-from datetime import datetime, timedelta
-from decimal import Decimal
-from random import randint, choice
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,13 +8,14 @@ from .serializer import ForecastSerializer
 
 @api_view(['GET'])
 def get_forecast(request):
-    return Response(ForecastSerializer(generate_mock_forecast("Florence")).data)
+    forecasts = Forecast.objects.all()
+    serializer = ForecastSerializer(forecasts, many=True)
+    return Response(serializer.data)
 
-def generate_mock_forecast(location):
-    now = datetime.now()
-    return Forecast(location=location,
-        date=now.date(),
-        time=now.time(),
-        temperature=Decimal(f"{randint(18, 35)}.{randint(0, 99):02}"),
-        description=choice(['Sunny', 'Rainy', 'Cloudy', 'Windy'])
-    )
+@api_view(['POST'])
+def create_forecast(request):
+    serializer = ForecastSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
